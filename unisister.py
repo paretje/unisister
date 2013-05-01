46f3cb62b1e7a2ade4463909c5ec259ec191f6de
Paretje <info@Online-Urbanus.be>
2013-05-01 19:02:01 +0000

    Check for configuration
    git-svn-id: http://www.online-urbanus.be/svn/unisister@10 988765b8-0798-4621-a01e-f0f98d6dfb67

--- unisister.py
+++ unisister.py
@@ -1,7 +1,7 @@
 # Unisister
 # Copyright: (C) 2013 Online - Urbanus
 # Website: http://www.Online-Urbanus.be
-# Last modified: 30/04/2013 by Paretje
+# Last modified: 01/05/2013 by Paretje
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,36 +17,42 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import wx
+
 import gettext
 from gettext import gettext as _
 import locale
-import config
 
 import threading
-import time
-import signal
-import sys
 import subprocess
-
 import os
 
+import config
+
 def error_dialog(message):
+	"""Display a dialog showing the given error message."""
 	dlg = wx.MessageDialog(None, message, _('Error'), wx.OK | wx.ICON_ERROR)
 	dlg.ShowModal()
 	dlg.Destroy()
 
 def get_icon(name, large=False):
-	# TODO: use at least os.path.join
-	# Even better would be to use, at least on linux, a more general system
-	# to get the right icon (so we can use theme-specific icons!)
+	"""
+	Get the icon specified by the name given, in the location specified in
+	the config.
+	
+	When the large flag is True, we will look in the path specified in the 
+	config as the path where the larger icons are located.
+	"""
+	# TODO: use theme-specific icons
 	if large:
-		return wx.Icon(config.ICON_LOCATION_LARGE + '/' + name + '.png',
-			wx.BITMAP_TYPE_PNG)
+		return wx.Icon(os.path.join(config.ICON_LOCATION_LARGE,
+			name + '.png'), wx.BITMAP_TYPE_PNG)
 	else:
-		return wx.Icon(config.ICON_LOCATION + '/' + name + '.png',
-			wx.BITMAP_TYPE_PNG)
+		return wx.Icon(os.path.join(config.ICON_LOCATION,
+			name + '.png'), wx.BITMAP_TYPE_PNG)
 
 class UnisisterPreferences(wx.Frame):
+	backends = ['unison']
+	
 	def __init__(self, task_bar):
 		wx.Frame.__init__(self, None, wx.ID_ANY, _("Unisister Preferences"))
 		self.task_bar = task_bar
@@ -56,7 +62,7 @@ class UnisisterPreferences(wx.Frame):
 		# TODO: Password?
 		# TODO: Add hover texts where needed
 		form_box = wx.BoxSizer(wx.VERTICAL)
-		form = wx.FlexGridSizer(cols=2, vgap=5, hgap=50)
+		form = wx.FlexGridSizer(cols=2, vgap=5, hgap=25)
 		form_save = wx.BoxSizer(wx.HORIZONTAL)
 		form.AddGrowableCol(1, 1)
 		
@@ -72,12 +78,20 @@ class UnisisterPreferences(wx.Frame):
 		form.Add(wx.StaticText(panel, label=_("Location on server:")), flag=wx.ALIGN_CENTER_VERTICAL)
 		form.Add(self.server_location, flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
 		
-		self.local_location = wx.FilePickerCtrl(panel, path=self.task_bar.config.Read('local_location'))
+		# Hmmm, seems like a bug in wxPython. The DirPickerCtrl doesn't
+		# seem to work, so we simply use the standard TextCtrl widget.
+		self.local_location = wx.TextCtrl(panel, value=self.task_bar.config.Read('local_location'))
 		form.Add(wx.StaticText(panel, label=_("Local folder:")), flag=wx.ALIGN_CENTER_VERTICAL)
 		form.Add(self.local_location, flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
 		
+		self.interval = wx.TextCtrl(panel, value=self.task_bar.config.Read('interval'))
+		form.Add(wx.StaticText(panel, label=_("Syncing interval:")), flag=wx.ALIGN_CENTER_VERTICAL)
+		form.Add(self.interval, flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
+		
+		self.backend = wx.Choice(panel, choices=UnisisterPreferences.backends)
+		self.backend.SetSelection(self.get_current_backend_id())
 		form.Add(wx.StaticText(panel, label=_("Backend:")), flag=wx.ALIGN_CENTER_VERTICAL)
-		form.Add(wx.StaticText(panel, label=_("Future option")), flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
+		form.Add(self.backend, flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
 		
 		self.backend_location_server = wx.TextCtrl(panel, value=self.task_bar.config.Read('backend_location_server'))
 		form.Add(wx.StaticText(panel, label=_("Backend location on server:")), flag=wx.ALIGN_CENTER_VERTICAL)
@@ -87,16 +101,15 @@ class UnisisterPreferences(wx.Frame):
 		form.Add(wx.StaticText(panel, label=_("Backend location on local machine:")), flag=wx.ALIGN_CENTER_VERTICAL)
 		form.Add(self.backend_location_local, flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
 		
-		# TODO: Add space between the two buttons
-		form_save.Add(wx.Button(panel, wx.ID_CANCEL, _("Cancel")))
-		form_save.Add(wx.Button(panel, wx.ID_OK, _("OK")))
+		form_save.Add(wx.Button(panel, wx.ID_CANCEL, _("Cancel")), flag=wx.ALL, border=10)
+		form_save.Add(wx.Button(panel, wx.ID_OK, _("OK")), flag=wx.ALL, border=10)
 		
 		form_box.Add(form, 0, flag=wx.EXPAND|wx.ALL, border=10)
 		form_box.Add(wx.StaticText(panel), 1)
-		form_box.Add(form_save, flag=wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, border=10)
+		form_box.Add(form_save, flag=wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, border=5)
 		panel.SetSizerAndFit(form_box)
 		
-		# Default width is to small, so only use height
+		# The calculated width is a bit small, so we only use the height
 		self.SetSize((500, self.GetBestSizeTuple()[1]))
 		
 		self.Bind(wx.EVT_BUTTON, self.OnCancel, id=wx.ID_CANCEL)
@@ -106,15 +119,39 @@ class UnisisterPreferences(wx.Frame):
 		self.Close(True)
 	
 	def OnOK(self, evt):
+		# Stop the timer, so it certainly doesn't start when changing
+		# the configuration.
+		self.task_bar.timer.Stop()
+		
+		# Primary preferences
 		self.task_bar.config.Write('server_address', self.server_address.GetValue())
 		self.task_bar.config.Write('server_username', self.server_username.GetValue())
 		self.task_bar.config.Write('server_location', self.server_location.GetValue())
-		self.task_bar.config.Write('local_location', self.local_location.GetPath())
-		#self.task_bar.config.Write('backend', self.backend.GetValue())
+		self.task_bar.config.Write('local_location', self.local_location.GetValue())
+		
+		# Test if the interval has been changed
+		try:
+			new_interval = int(self.interval.GetValue())
+			self.task_bar.config.WriteInt('interval', new_interval)
+		except ValueError:
+			# TODO: Show error
+			return
+		
+		# Backend preferences
+		self.task_bar.config.Write('backend', UnisisterPreferences.backends[self.backend.GetSelection()])
 		self.task_bar.config.Write('backend_location_server', self.backend_location_server.GetValue())
 		self.task_bar.config.Write('backend_location_local', self.backend_location_local.GetPath())
 		self.task_bar.config.Flush()
 		self.Close(True)
+		if self.task_bar.timer_should_be_running:
+			self.task_bar.start_timer()
+	
+	def get_current_backend_id(self):
+		try:
+			return UnisisterPreferences.backends.index(self.task_bar.config.Read('backend'))
+		except:
+			# Get unison backend by default
+			return UnisisterPreferences.backends.index('unison')
 
 class UnisisterThread(threading.Thread):
 	_bussy = False
@@ -144,33 +181,47 @@ class UnisisterThread(threading.Thread):
 		
 		# Start backend
 		if self.task_bar.config.Read('backend') == 'csync':
-			wx.CallAfter(error_dialog, _("Using csync as a backend is not supported by the current version of Unisister yet."))
+			wx.CallAfter(error_dialog, _("Using csync as a backend is not supported by the current version of Unisister yet, but there is an intention to implement this later."))
 		else:
-			arguments = []
-			
-			# Test if the user has set a username
-			if self.task_bar.config.Read('server_username') != "":
-				server_address = self.task_bar.config.Read('server_username') + '@' + self.task_bar.config.Read('server_address')
-			else:
-				server_address = self.task_bar.config.Read('server_address')
-			
-			# Test if the user has set a specific server unison executable
-			if self.task_bar.config.Read('backend_location_server') != "":
-				arguments += ['-servercmd', self.task_bar.config.Read('backend_location_server')]
-			
-			# Test if the user has set a specific local unison executable
-			if self.task_bar.config.Read('backend_location_local') != "":
-				unison_local = self.task_bar.config.Read('backend_location_local')
-			else:
-				unison_local = 'unison'
-			
-			# Start the unison process
-			server = 'ssh://' + server_address + '/' + self.task_bar.config.Read('server_location')
-			local = self.task_bar.config.Read('local_location')
-			subprocess.call([unison_local, server, local, '-batch', '-prefer', server] + arguments)
+			self.unison_backend()
 		
 		self.task_bar.SetIcon(get_icon(config.ICON_IDLE), "Unisister")
 		UnisisterThread._bussy = False
+	
+	def unison_backend(self):
+		arguments = []
+		
+		# Test if the needed configuration is set
+		if self.task_bar.config.Read('server_address') == "" \
+		or self.task_bar.config.Read('local_location') == "" \
+		or not os.path.isdir(self.task_bar.config.Read('local_location')):
+			wx.CallAfter(self.no_configuration)
+			return
+		
+		# Test if the user has set a username
+		if self.task_bar.config.Read('server_username') != "":
+			server_address = self.task_bar.config.Read('server_username') + '@' + self.task_bar.config.Read('server_address')
+		else:
+			server_address = self.task_bar.config.Read('server_address')
+		
+		# Test if the user has set a specific server unison executable
+		if self.task_bar.config.Read('backend_location_server') != "":
+			arguments += ['-servercmd', self.task_bar.config.Read('backend_location_server')]
+		
+		# Test if the user has set a specific local unison executable
+		if self.task_bar.config.Read('backend_location_local') != "":
+			unison_local = self.task_bar.config.Read('backend_location_local')
+		else:
+			unison_local = 'unison'
+		
+		# Start the unison process
+		server = 'ssh://' + server_address + '/' + self.task_bar.config.Read('server_location')
+		subprocess.call([unison_local, server, self.task_bar.config.Read('local_location'), '-batch', '-prefer', server] + arguments)
+	
+	def no_configuration(self):
+		self.task_bar.timer.Stop()
+		error_dialog(_("Unisister has not been properly configurated. Open the preferences and fill in the necesarry configuration."))
+		self.task_bar.ShowPreferences(None)
 	
 	# Not very Pythonic, but a property can't be static, and I don't want
 	# to use the variable directly, as I think it's quite likely this
@@ -182,6 +233,7 @@ class UnisisterThread(threading.Thread):
 
 class UnisisterTaskBar(wx.TaskBarIcon):
 	ID_SYNCH = wx.NewId()
+	timer_should_be_running = True
  
 	def __init__(self):
 		wx.TaskBarIcon.__init__(self)
@@ -191,10 +243,6 @@ class UnisisterTaskBar(wx.TaskBarIcon):
  		
 		# Set the icon of Unisister
 		self.SetIcon(get_icon(config.ICON_IDLE), "Unisister")
- 		
- 		# Create Timer-object, and keep it, as it's necessarry to let in function properly (GBC?)
- 		self.timer = wx.Timer(self, UnisisterTaskBar.ID_SYNCH)
- 		self.Synchronisation(None)
  		
 		# Bind the events
 		self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.OnTaskBarLeftClick)
@@ -209,10 +257,10 @@ class UnisisterTaskBar(wx.TaskBarIcon):
 		self.Bind(wx.EVT_MENU, self.ShowPreferences, id=wx.ID_PROPERTIES)
 		self.Bind(wx.EVT_MENU, self.AboutUnisister, id=wx.ID_ABOUT)
 		
-		self.Bind(wx.EVT_TIMER, self.Synchronisation, id=UnisisterTaskBar.ID_SYNCH)
-		
-		# Event generator in order to synchronize every n seconds
-		self.timer.Start(120*1000)
+ 		# Create Timer-object, and keep it, as it's necessarry to let in function properly (GBC?)
+ 		self.timer = wx.Timer(self, UnisisterTaskBar.ID_SYNCH)
+		self.timer.Bind(wx.EVT_TIMER, self.Synchronisation, id=UnisisterTaskBar.ID_SYNCH)
+		self.start_timer()
  
 	def CreatePopupMenu(self, evt=None):
 		menu = wx.Menu()
@@ -244,7 +292,6 @@ class UnisisterTaskBar(wx.TaskBarIcon):
 	
 	def ShowPreferences(self, evt):
 		UnisisterPreferences(self).Show()
-		
 	
 	def AboutUnisister(self, evt):
 		info = wx.AboutDialogInfo()
@@ -260,6 +307,13 @@ class UnisisterTaskBar(wx.TaskBarIcon):
 		except Exception:
 			info.License = _("Released under the terms of the GNU General Public License Version 3 \nSee http://www.gnu.org/licenses/gpl.html for more details.")
 		wx.AboutBox(info)
+	
+	def start_timer(self):
+		self.Synchronisation(None)
+		if self.config.ReadInt('interval') > 0:
+			self.timer.Start(self.config.ReadInt('interval')*1000)
+		else:
+			self.timer.Start(120*1000)
 
 def _set_localisation():
 	# Code based on the code in system-config-printer v1.3.7
